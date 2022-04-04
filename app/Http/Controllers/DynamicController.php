14c482ee42c43843f47371b3699b20c860b06ed4
naotarian <naotarian19@gmail.com>
2022-04-04 10:09:24 +0000

    phpから.pyを実行

---
+++ app/Http/Controllers/DynamicController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class DynamicController extends Controller
+{
+    public function test() {
+        $path =  app_path() . "/Library/Python/text_extract.py 2>&1";
+        $command="python " . $path;
+        exec($command, $output, $return);
+    }
+}
